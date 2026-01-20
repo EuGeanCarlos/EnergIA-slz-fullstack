e223385c111758bd5fcbaeefc119acd5a5764ca8
Gean Carlos <geancarlsilva@gmail.com>
2026-01-19 23:07:04 -0300

    feat: adiciona metodo findByUsuarioId no ConsumoRepository

--- energiaslz/src/main/java/br/com/energia/energiaslz/repository/ConsumoRepository.java
+++ energiaslz/src/main/java/br/com/energia/energiaslz/repository/ConsumoRepository.java
@@ -3,5 +3,10 @@ package br.com.energia.energiaslz.repository;
 import br.com.energia.energiaslz.model.Consumo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ConsumoRepository extends MongoRepository<Consumo, String> {
+
+    List<Consumo> findByUsuarioId(String usuarioId);
+
 }
