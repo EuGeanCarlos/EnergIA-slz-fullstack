d19b2dca94ae0b52197a158a51628dd6893b870d
Gean Carlos <geancarlsilva@gmail.com>
2026-01-20 10:41:39 -0300

    RelatorioController

---
+++ energiaslz/src/main/java/br/com/energia/energiaslz/controller/RelatorioController.java
@@ -0,0 +1,27 @@
+package br.com.energia.energiaslz.controller;
+
+import br.com.energia.energiaslz.dto.RelatorioDTO;
+import br.com.energia.energiaslz.service.RelatorioService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/relatorios")
+@CrossOrigin(origins = "*")
+public class RelatorioController {
+
+    private final RelatorioService relatorioService;
+
+    public RelatorioController(RelatorioService relatorioService) {
+        this.relatorioService = relatorioService;
+    }
+
+    @GetMapping("/{usuarioId}")
+    public RelatorioDTO gerarRelatorio(@PathVariable String usuarioId) {
+        return relatorioService.gerarRelatorioPorUsuario(usuarioId);
+    }
+    @GetMapping
+    public String info() {
+        return "Informe o ID do usuário na URL.";
+    }
+
+}
